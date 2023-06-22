72482f06b8988c63f24328e1bc5203b99a895adf
amine-niouar <niouar.amin@gmail.com>
2023-06-22 20:40:34 +0100

    trying to add redis

--- wordpress-like/script.sh
+++ wordpress-like/script.sh
@@ -2,6 +2,11 @@
 #service php8.2-fpm start 
 wp core install  --title=shit --admin_user=root --admin_password=root --admin_email=shit@shit.com --allow-root --path=/var/www/html/ --url=localhost 
 wp theme install twentysixteen --activate --allow-root --path=/var/www/html/
+wp plugin install redis-cache --activate --allow-root --path=/var/www/html/
+wp redis enable --allow-root --path=/var/www/html/
+wp config set WP_REDIS_HOST "redis-cache" --allow-root --path=/var/www/html/
+wp config set WP_REDIS_PORT "6379" --allow-root --path=/var/www/html/
+wp config set WP_REDIS_DATABASE "15" --allow-root --path=/var/www/html/
 #wp theme install twentysixteen --activate  --admin_user=root --admin_password=root --admin_email=shit@shit.com --allow-root --path=/var/www/html/ --url=localhost 
 
 # exec tail -f
