ae920bcc782fe9992e68296d4a4242d4f317b63b
Amine Niouar <niouar.amin@gmail.com>
2023-07-10 20:41:25 -0400

    with ftp

--- ftpserver/scripts.sh
+++ ftpserver/scripts.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-adduser talker
-echo "talker:root" | chpasswd
- mkdir /var/www/ && mkdir /var/www/html
- chown -R talker: /var/www/html
- chmod 550 /var/www/ && chmod 550 /var/www/html/
- echo "talker" |  tee -a /etc/vsftpd.userlist
-  touch /var/log/vsftpd.log
-ufw allow 21
- service vsftpd start
+#adduser talker
+#echo "talker:root" | chpasswd
+ #mkdir /var/www/ && mkdir /var/www/html
+ #chown -R talker: /var/www/html
+ #chmod 550 /var/www/ && chmod 550 /var/www/html/
+ #echo "talker" |  tee -a /etc/vsftpd.userlist
+  #touch /var/log/vsftpd.log
+#ufw allow 21
+# service vsftpd start
+mkdir -p /var/run/vsftpd/empty
+chmod 755 /var/run/vsftpd/empty
+useradd -m -p $(openssl passwd -1 toor) talker
 
+echo "talker:toor" | chpasswd
+chown -R talker /var/www/html
 
-exec vsftpd
+/usr/sbin/vsftpd /etc/vsftpd.conf
